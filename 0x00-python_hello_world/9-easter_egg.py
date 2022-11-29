969c3e1f9886469fd427cfff4c60ecbd753a333a
Nusultkang1 <nurudeensultan28@gmail.com>
2022-11-29 20:59:51 +0100

    Task

--- 0x00-python_hello_world/9-easter_egg.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python3
-print("The Zen of Python, by Tim Peters\
-        \
-        Beautiful is better than ugly.\
-        Explicit is better than implicit.\
-        Simple is better than complex.\
-        Complex is better than complicated.")
