faa56992215bf5417b5ccd2480c17a46727ed684
Nusultkang1 <nurudeensultan28@gmail.com>
2022-12-05 22:36:55 +0100

    Second commit

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,8 +1,4 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    i = 0
-
-    while (i < len(my_string)):
-        if (my_string[i] == 'c' and my_string[i] == 'C'):
-            my_string[i] = ''
-            i = i + 1
+    new_string = my_string.translate({ord(i): None for i in 'cC'})
+    return new_string
